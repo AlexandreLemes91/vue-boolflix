0a37798efaa1ad72f32525fb8a99fe1316aeb141
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-05-10 19:33:25 +0200

    bozza contenuto

---
+++ src/components/Content.vue
@@ -0,0 +1,43 @@
+<template>
+    <main>
+        <ul v-for="video in videoList" :key="video.id">
+            <li>Title: {{video.title}}</li>
+            <li>Original Title: {{video.original_title}}</li>
+            <li>Language: {{video.original_language}}</li>
+            <li>{{video.vote_average}}</li>
+        </ul>
+    </main>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Content',
+
+    data(){
+        return{
+            apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=24bf6c82b0fc8353485988e42545f9c0&query=v for vendetta',
+            videoList: [],
+        }
+    },
+
+    created(){
+        this.getList();
+    },
+
+    methods: {
+        getList(){
+            axios.get(this.apiURL)
+            .then(resp=>{
+                this.videoList = resp.data.results;
+                console.log(this.videoList);
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <form>
+            <input type="text">
+            <button>Search</button>
+        </form>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang='scss'>
+
+header{
+    background-color: #555;
+    padding: 20px;
+}
+
+
+</style>
